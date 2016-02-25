7cc821d23848fe70ec501c6ed2da5a565ca5aa0e
Richard Payne <richy_payne@hotmail.co.uk>
2016-02-25 10:28:21 +0000

    html added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -126,3 +126,20 @@ $(document).ready(function() {
 
 });
 
+// functionality to show/hide filters
+function toggleFilters(){
+  var filters = $('#filters-holder');
+  var filterToggle = $('#filterToggle');
+  filters.toggle();
+
+  if (filters.is(':visible'))
+  {
+    filterToggle.removeClass('filters-hidden');
+    filterToggle.addClass('filters-showing');
+  } else {
+    filterToggle.removeClass('filters-showing');
+    filterToggle.addClass('filters-hidden');
+  }
+
+}
+
