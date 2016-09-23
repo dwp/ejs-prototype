c084f1808022894f572847490e10f93340a950e6
garyboyle <gary@shayoo.co.uk>
2016-09-23 15:19:46 +0100

    Adding in the history view

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,26 @@
-module.exports = {
-  bind : function (app) {
+var express = require('express');
+var router = express.Router();
 
-    app.get('/', function (req, res) {
-      res.render('index');
-    });
+router.get('/', function (req, res) {
+  res.render('index');
+});
 
-    // add your routes here
+// Example route: Passing data into a page
+router.get('/examples/template-data', function (req, res) {
+  res.render('examples/template-data', { 'name' : 'Foo' });
+});
 
-     app.get('/admin', function (req, res) {
-      res.render('admin');
-    });
+// add your routes here
+router.use(function(req, res, next){
+  Object.assign(res.locals,{
+    postData: (req.body ? req.body : false)
+  });
 
+  Object.assign(res.locals,{
+    getData: (req.query ? req.query : false)
+  });
 
-  }
-};
+  next();
+});
+
+module.exports = router;
