62a0f0806a2eee2c836d3c8ba85921df41c39768
garyboyle <gary@shayoo.co.uk>
2016-10-07 09:21:59 +0100

    Adding the playback of the provision.

--- app/routes.js
+++ app/routes.js
@@ -5,9 +5,23 @@ router.get('/', function (req, res) {
   res.render('index');
 });
 
-// Example route: Passing data into a page
-router.get('/examples/template-data', function (req, res) {
-  res.render('examples/template-data', { 'name' : 'Foo' });
+router.get('/latest/provision', function (req, res) {
+
+  req.session.provisions = req.session.provisions || getTestProvisions();
+
+  res.render('latest/provision', {
+  	provisions: req.session.provisions
+  });
+});
+
+router.get('/latest/provision_edit', function (req, res) {
+
+  var provision = req.session.provisions ? req.session.provisions[req.query.id] : getTestProvisions()[req.query.id];
+  //var provision = getTestProvisions()[req.query.id];
+
+  res.render('latest/provision_edit', {
+    provision: provision
+  });
 });
 
 // add your routes here
@@ -22,5 +36,13 @@ router.use(function(req, res, next){
 
   next();
 });
+
+function getTestProvisions(){
+  return [
+    { id: 0, type : 'Work experience', startDateValues: [ '01', '08', '2016'], startDate: '1 August 2016', status: 'Confirmed', endDateValues: ['04', '09', '2016'], endDate: '4 September 2016', outcome: 'Dismissed'},
+    { id: 1, type : 'Work experience', startDateValues: [ '01', '05', '2016'], startDate: '1 May 2016', status: 'Did not start', endDateValues: ['', '', ''], endDate: '', outcome: ''},
+    { id: 2, type : 'Work experience', startDateValues: [ '01', '01', '2016'], startDate: '1 January 2016', status: 'Confirmed', endDateValues: ['31', '04', '2016'], endDate: '31 April 2016', outcome: 'Completed'}
+  ];
+}
 
 module.exports = router;
