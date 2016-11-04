d8ff941d0655702ad48c2a1a3464758e52c72c9d
garyboyle <gary@shayoo.co.uk>
2016-11-04 15:51:34 +0000

    Improve the routing of the toggle user logic.

--- app/routes.js
+++ app/routes.js
@@ -72,14 +72,20 @@ router.post('/latest/provision_edit', function (req, res) {
 });
 
 router.get('/latest/toggle_user_role', function(req, res, next){
+  var fromIndexPage = !req.header('Referer').includes('latest');
+
   if(req.session.user && req.session.user.role && req.session.user.role !== 'manager'){
     req.session.user = { name : 'Martha Vansant', role : 'manager' }
   } else {
     req.session.user = { name : 'William Conroy', role : 'workcoach' }
   }
 
-  res.redirect('/')
-})
+  if(fromIndexPage){
+    res.redirect('/');
+  } else {
+    res.redirect('/latest/job');
+  }
+});
 
 function getTestProvisions(){
   return [ 
