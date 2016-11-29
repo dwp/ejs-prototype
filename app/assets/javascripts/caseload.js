d56a0e0ac082f283b49ba12d0bb7116b055cb20b
garyboyle <gary@shayoo.co.uk>
2016-11-29 16:02:47 +0000

    Fix the filter option for the performance screen

--- app/assets/javascripts/caseload.js
+++ app/assets/javascripts/caseload.js
@@ -1,13 +1,29 @@
 $(function(){
-	$('#search-caseload').on('click', function(e){
-		e.preventDefault();
+  var $extraOption = $('#extraCaseloadSegmentationOption');
 
-		var provisionSelected = $('#provision-type').find('option:selected').val();
+  toggleCaseloadSegmentationOptions();
 
-		if(provisionSelected){
-			window.location = 'performance_jsa?provisionSelected=true'
-		} else {
-			window.location = 'performance_jsa'
-		}
-	})	
+  $('#search-caseload').on('click', function(e){
+    e.preventDefault();
+
+    var provisionSelected = $('#provision-type').find('option:selected').val();
+
+    if(provisionSelected){
+      window.location = 'performance_jsa?provisionSelected=true'
+    } else {
+      window.location = 'performance_jsa'
+    }
+  });
+
+  function toggleCaseloadSegmentationOptions(){
+    var type = $('#benefit').find('option:selected').data('type');
+
+    if(type === 'IS' || type === 'ESA'){
+      $extraOption.show()
+    } else {
+      $extraOption.hide()
+    }
+  }
+
+  $('#benefit').on('change', toggleCaseloadSegmentationOptions);
 })
