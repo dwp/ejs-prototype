d3ba8e2528e7fb5380cfe222c4bfc58ff19c001e
garyboyle <gary@shayoo.co.uk>
2016-12-01 09:55:21 +0000

    Rebrand of EJS to DCAMS

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Enhanced Jobseeker Services',
+  serviceName: 'Digital Claimant and Activity Messaging Service',
 
   // Default port that prototype runs on
   port: '3000',
