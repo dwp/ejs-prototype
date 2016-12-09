41835d7df4e36c6aaf2bf3db96438f8cb0b11a2a
garyboyle <gary@shayoo.co.uk>
2016-12-09 14:50:48 +0000

    IS child age support for the caseload screen

--- app/assets/javascripts/caseload.js
+++ app/assets/javascripts/caseload.js
@@ -1,19 +1,24 @@
 $(function(){
-  var $extraOption = $('#extraCaseloadSegmentationOption');
-  var $dayOneEvidenceFilter = $('#day-one-evidence-filter')
+  var $extraOption = $('#extraCaseloadSegmentationOption'),
+    $dayOneEvidenceFilter = $('#day-one-evidence-filter'),
+    $childAgeFilter = $('#child-age-filter');
 
   toggleCaseloadSegmentationOptions();
   toggleDayOneEvidenceFilter();
+  toggleChildAgeFilter();
 
   $('#search-caseload').on('click', function(e){
     e.preventDefault();
 
-    var provisionSelected = $('#provision-type').find('option:selected').val();
+    var provisionSelected = $('#provision-type').find('option:selected').val(),
+      benefitSelected = $('#benefit').find('option:selected').data('type'),
+      childAgeSelected = $('#child-age-select').find('option:selected').val(),
+      action = 'performance_' + (benefitSelected.toLowerCase() || 'jsa' ) + '?childAge=' + childAgeSelected;
 
     if(provisionSelected){
-      window.location = 'performance_jsa?provisionSelected'
+      window.location = action + '&provisionSelected';
     } else {
-      window.location = 'performance_jsa'
+      window.location = action;
     }
   });
 
@@ -37,8 +42,19 @@ $(function(){
     }
   }
 
+  function toggleChildAgeFilter(){
+    var type = $('#benefit').find('option:selected').data('subtype');
+
+    if(type === 'lone-parent'){
+      $childAgeFilter.show();
+    } else{
+      $childAgeFilter.hide();
+    }
+  }
+
   $('#benefit').on('change', function(){
     toggleCaseloadSegmentationOptions();
     toggleDayOneEvidenceFilter();
+    toggleChildAgeFilter();
   });
 })
