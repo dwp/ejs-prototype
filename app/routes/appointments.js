1021b3896dee92e1224ba5c11b0203f0629e27ab
garyboyle <gary@shayoo.co.uk>
2017-01-23 15:07:48 +0000

    Use session data for the appointments summary

--- app/routes/appointments.js
+++ app/routes/appointments.js
@@ -2,9 +2,9 @@ module.exports = function(router){
   router.post('/latest/job_next_new', function(req, res){
 
     if(req.body.appointmentUpdate){
-    req.session.hasAppointment = false;
+      req.session.hasAppointment = false;
     } else {
-    req.session.hasAppointment = true;
+      req.session.hasAppointment = true;
     }
 
     res.render('latest/job_next_new');
