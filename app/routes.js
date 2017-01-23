1166e5871d56dee61a0052905e470895f33478dc
garyboyle <gary@shayoo.co.uk>
2017-01-23 11:40:37 +0000

    Further tidy up of the routes

--- app/routes.js
+++ app/routes.js
@@ -1,24 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(function(req, res, next){
-  Object.assign(res.locals,{
-    postData: (req.body ? req.body : false)
-  });
-
-  Object.assign(res.locals,{
-    getData: (req.query ? req.query : false)
-  });
-
-  req.session.user = req.session.user || { name : 'Martha Vansant', role : 'manager' };
-  
-  Object.assign(res.locals,{
-    sessionData: (req.session ? req.session : false)
-  });
-
-  next();
-});
-
 require('./routes/init')(router);
 require('./routes/index')(router);
 require('./routes/provision')(router);
