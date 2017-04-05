111ebde31366c780dfaba9b348415917b033671f
garyboyle <gary@shayoo.co.uk>
2017-04-05 15:24:12 +0100

    Copy over v6 of the prototype kit

--- app/config.js
+++ app/config.js
@@ -16,6 +16,8 @@ module.exports = {
   useHttps: 'true',
 
   // Cookie warning - update link to service's cookie page.
-  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>'
-
+  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>',
+  
+  // Enable or disable Browser Sync
+  useBrowserSync: 'true'
 }
