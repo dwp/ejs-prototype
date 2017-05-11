139211b5e770b6186363ad195294ac12f457f98a
garyboyle <gary@shayoo.co.uk>
2017-05-11 14:28:07 +0100

    Rejig the prototype for whp selection tool

--- app/routes/selection-tool.js
+++ app/routes/selection-tool.js
@@ -1,73 +1,86 @@
 module.exports = function(router) {
   var fs = require('fs');
-  var questions = {};
+  var content = loadContent();
 
-  fs.readdir(__dirname + '/whp-selection-questions', function(err, filenames){
-    if(err) return;
-
-    filenames.forEach(function(filename){
-      questions[filename.split('.')[0]] = require('./whp-selection-questions/' + filename);
-    })
-  })
-
+  // Landing page routes
   router.get('/latest/selection_tool', function (req, res) {
-    res.locals.questions = questions['claimant-group'].questions;
-    res.locals.next = questions['claimant-group'].nextQuestionSet;
-    res.locals.questionTitle = questions['claimant-group'].title;
-    res.locals.earlyEntryOptions = questions['claimant-group'].earlyEntryOptions;
+    res.locals.questions = content['landing-page'].questions;
+    res.locals.questionTitle = content['landing-page'].title;
+    res.locals.earlyEntryOptions = content['landing-page'].earlyEntryOptions;
 
     res.render('latest/selection_tool_landing_page');
-  });
+  });   
+    
+  router.post('/latest/selection_tool', function (req, res) {
+    var isLongTermUnemployed = req.body['initialOptions'] === 'Long term unemployed',
+      isExternalReferrer =  req.body['voluntary'] === 'External referral';
 
-   // Alternative landing page route for GET
-   router.get('/latest/selection_tool_landing_page_v2', function (req, res) {
-    res.locals.questions = questions['claimant-group'].questions;
-    res.locals.next = questions['claimant-group'].nextQuestionSet;
-    res.locals.questionTitle = questions['claimant-group'].title;
-    res.locals.earlyEntryOptions = questions['claimant-group'].earlyEntryOptions;
+    if(isLongTermUnemployed){
+      res.redirect('/latest/whp-result');
+    } else if (isExternalReferrer){
+      res.redirect('/latest/scoring_questions/0');
+    } else {
+      res.redirect('/latest/whp_eligibility_questions?voluntaryOption=' + req.body.voluntary);
+    }
+  });
+  
+  // Alternative landing page route for GET
+  router.get('/latest/selection_tool_landing_page_v2', function (req, res) {
+    res.locals.questions = content['landing-page'].questions;
+    res.locals.questionTitle = content['landing-page'].title;
+    res.locals.earlyEntryOptions = content['landing-page'].earlyEntryOptions;
 
     res.render('latest/selection_tool_landing_page_v2');
   });
+    
+  // Alternative landing page route for POST
+  router.post('/latest/selection_tool_landing_page_v2', function (req, res) {
+    var isLongTermUnemployed = req.body['initialOptions'] === 'Long term unemployed',
+      isExternalReferrer =  req.body['voluntary'] === 'External referral';
 
-  router.post('/latest/selection_tool', function (req, res) {
-    var questionGroup = req.body['claimant-group'] || 'voluntary';
-
-    if(questionGroup=== 'long-term-unemployed'){
+    if(isLongTermUnemployed){
       res.redirect('/latest/whp-result');
+    } else if (isExternalReferrer){
+      res.redirect('/latest/scoring_questions/0');
     } else {
-      res.redirect('/latest/selection_tool/' + questionGroup);
+      res.redirect('/latest/whp_eligibility_questions?voluntaryOption=' + req.body.voluntary);
     }
   });
 
-  // Alternative landing page route for POST
-  router.post('/latest/selection_tool_landing_page_v2', function (req, res) {
-      var questionGroup = req.body['claimant-group'] || 'voluntary';
+  // Eligibility routes
+  router.get('/latest/whp_eligibility_questions', function (req, res) {
+    var questionGroup = req.query.voluntaryOption === 'Disabled' ? 'disability' : 'early-entry';
 
-      if(questionGroup=== 'long-term-unemployed'){
-          res.redirect('/latest/whp-result');
-      } else {
-          res.redirect('/latest/selection_tool/' + questionGroup);
+    res.locals.questions = content[questionGroup].questions;
+    res.locals.questionTitle = content[questionGroup].title;
+    res.locals.earlyEntryOptions = content[questionGroup].earlyEntryOptions;
+
+    res.render('latest/whp_eligibility_questions');
+  });
+
+  router.post('/latest/whp_eligibility_questions', function (req, res) {
+    var isEligible = true;
+
+    // If any of the eligibility questions have been answered 'No' 
+    // then the applicant is no eligible
+    for(var param in req.body){
+      if(req.body[param] === 'No'){
+        isEligible = false;
       }
+    }
+
+    if(isEligible){
+      res.redirect('/latest/scoring_questions/0');
+    } else {
+      res.redirect('/latest/failed-eligibility');
+    }
   });
 
-  router.post('/latest/selection_tool/*', function (req, res) {
-    res.redirect('/latest/selection_tool_v2/0');
-  });
-
-  router.get('/latest/selection_tool/:questionSet', function (req, res) {
-    res.locals.questions = questions[req.params.questionSet].questions;
-    res.locals.next = questions[req.params.questionSet].nextQuestionSet;
-    res.locals.questionTitle = questions[req.params.questionSet].title;
-    res.locals.explanation = questions[req.params.questionSet].explanation;
-
-    res.render('latest/selection_tool');
-  });
-
-  router.get('/latest/selection_tool_v2/:questionSet', function (req, res) {
-    var scoringQuestionsConfig = questions['scoring-questions-v2'];
+  // Scoring question routes
+  router.get('/latest/scoring_questions/:questionSet', function (req, res) {
+    var scoringQuestionsConfig = content['scoring-questions'];
 
     res.locals.questions = [scoringQuestionsConfig.questions[req.params.questionSet]];
-    res.locals.next = scoringQuestionsConfig.nextQuestionSet;
     res.locals.questionTitle = scoringQuestionsConfig.title;
     res.locals.questionIndex = +req.params.questionSet + 1;
     res.locals.questionCount = scoringQuestionsConfig.questions.length;
@@ -77,8 +90,22 @@ module.exports = function(router) {
     if(req.params.questionSet == scoringQuestionsConfig.questions.length){
       res.redirect('/latest/whp-result');
     } else {
-      res.locals.formAction = `/latest/selection_tool_v2/${++req.params.questionSet}`;
-      res.render('latest/selection_tool');
+      res.locals.formAction = `/latest/scoring_questions/${++req.params.questionSet}`;
+      res.render('latest/scoring_questions');
     }
   });
+
+  function loadContent(){
+    var content = {};
+
+    fs.readdir(__dirname + '/whp-selection-questions', function(err, filenames){
+      if(err) return;
+
+      filenames.forEach(function(filename){
+        content[filename.split('.')[0]] = require('./whp-selection-questions/' + filename);
+      })
+    })
+
+    return content;
+  }
 }
