f5da9af8951e6cc5fa816094c7d5bc0703a39229
janegleadall <jane.gleadall.java@gmail.com>
2017-05-25 17:54:39 +0100

    DO NOT merge into master. FATAL ERROR to be resolved. Trying to make weekly profile rows iterative rather thtan hard-coded numbered weeks.

--- app/controllers/whpGatekeeperController.js
+++ app/controllers/whpGatekeeperController.js
@@ -46,9 +46,9 @@ function districtProfileAction(req, res) {
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-/*                                        District Weekly Profile Controllers
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-*/
+ /*                                        District Weekly Profile Controllers
+ /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
 function districtWeeklyProfilePage (req, res) {
 
   req.session.profileData = req.session.profileData || {};
@@ -56,90 +56,28 @@ function districtWeeklyProfilePage (req, res) {
 
   var whpFlatProfile = 0;
   var pscFlatprofile = 0;
-  var week1whpProfile= 0,
-      week2whpProfile = 0,
-      week3whpProfile = 0,
-      week4whpProfile = 0,
-      week5whpProfile = 0,
-      week52whpProfile = 0,
-      week1pscProfile = 0,
-      week2pscProfile = 0,
-      week3pscProfile = 0,
-      week4pscProfile = 0,
-      week5pscProfile = 0,
-      week52pscProfile = 0,
-      week1whpExtrasProfile = 0,
-      week2whpExtrasProfile = 0,
-      week3whpExtrasProfile = 0,
-      week4whpExtrasProfile = 0,
-      week5whpExtrasProfile = 0,
-      week52whpExtrasProfile = 0,
-      week1pscExtrasProfile = 0,
-      week2pscExtrasProfile = 0,
-      week3pscExtrasProfile = 0,
-      week4pscExtrasProfile = 0,
-      week5pscExtrasProfile = 0,
-      week52pscExtrasProfile = 0;
-
-  if (!req.session.profileData.totalPlaces && !req.session.weeklyProfileData.week1whpProfile) {
-    whpFlatProfile = Math.round(1300 / 52);
-    pscFlatProfile = Math.round(100 / 52);
-  } else {
-    whpFlatProfile = Math.round(req.session.profileData.whpProfile / 52);
-    pscFlatProfile = Math.round(req.session.profileData.pscProfile / 52)
+  var inputFullYearProfileData = [];
+  var fullYearProfileData = [];
+  var weekProfileData = {
+    weekwhpProfile : 0,
+    weekpscProfile : 0,
+    weekwhpExtrasProfile : 0,
+    weekpscExtrasProfile : 0
   };
 
-  // If nothing already saved in weeklyProfileData
-  if (!req.session.weeklyProfileData.week1whpProfile) {
-    week1whpProfile = whpFlatProfile;
-    week2whpProfile = whpFlatProfile;
-    week3whpProfile = whpFlatProfile;
-    week4whpProfile = whpFlatProfile;
-    week5whpProfile = whpFlatProfile;
-    week52whpProfile = whpFlatProfile;
-    week1pscProfile = pscFlatProfile;
-    week2pscProfile = pscFlatProfile;
-    week3pscProfile = pscFlatProfile;
-    week4pscProfile = pscFlatProfile;
-    week5pscProfile = pscFlatProfile;
-    week52pscProfile = pscFlatProfile;
-    week1whpExtrasProfile = 0;
-    week2whpExtrasProfile = 0;
-    week3whpExtrasProfile = 0;
-    week4whpExtrasProfile = 0;
-    week5whpExtrasProfile = 0;
-    week52whpExtrasProfile = 0;
-    week1pscExtrasProfile = 0;
-    week2pscExtrasProfile = 0;
-    week3pscExtrasProfile = 0;
-    week4pscExtrasProfile = 0;
-    week5pscExtrasProfile = 0;
-    week52pscExtrasProfile = 0;
+  if (isFalsey(req.session.profileData) || isFalsey(req.session.weeklyProfileData)) {
+    whpFlatProfile = Math.round(req.session.profileData.whpProfile / 52);
+    pscFlatProfile = Math.round(req.session.profileData.pscProfile / 52);
   } else {
-    week1whpProfile = req.session.weeklyProfileData.week1whpProfile;
-    week2whpProfile = req.session.weeklyProfileData.week2whpProfile;
-    week3whpProfile = req.session.weeklyProfileData.week3whpProfile;
-    week4whpProfile = req.session.weeklyProfileData.week4whpProfile;
-    week5whpProfile = req.session.weeklyProfileData.week5whpProfile;
-    week52whpProfile = req.session.weeklyProfileData.week52whpProfile;
-    week1pscProfile = req.session.weeklyProfileData.week1pscProfile;
-    week2pscProfile = req.session.weeklyProfileData.week2pscProfile;
-    week3pscProfile = req.session.weeklyProfileData.week3pscProfile;
-    week4pscProfile = req.session.weeklyProfileData.week4pscProfile;
-    week5pscProfile = req.session.weeklyProfileData.week5pscProfile;
-    week52pscProfile = req.session.weeklyProfileData.week52pscProfile;
-    week1whpExtrasProfile = req.session.weeklyProfileData.week1whpExtrasProfile;
-    week2whpExtrasProfile = req.session.weeklyProfileData.week2whpExtrasProfile;
-    week3whpExtrasProfile = req.session.weeklyProfileData.week3whpExtrasProfile;
-    week4whpExtrasProfile = req.session.weeklyProfileData.week4whpExtrasProfile;
-    week5whpExtrasProfile = req.session.weeklyProfileData.week5whpExtrasProfile;
-    week52whpExtrasProfile = req.session.weeklyProfileData.week52whpExtrasProfile;
-    week1pscExtrasProfile = req.session.weeklyProfileData.week1pscExtrasProfile;
-    week2pscExtrasProfile = req.session.weeklyProfileData.week2pscExtrasProfile;
-    week3pscExtrasProfile = req.session.weeklyProfileData.week3pscExtrasProfile;
-    week4pscExtrasProfile = req.session.weeklyProfileData.week4pscExtrasProfile;
-    week5pscExtrasProfile = req.session.weeklyProfileData.week5pscExtrasProfile;
-    week52pscExtrasProfile = req.session.weeklyProfileData.week52pscExtrasProfile;
+    whpFlatProfile = Math.round(1300 / 52);
+    pscFlatProfile = Math.round(100 / 52);
+  };
+
+  // Check if data already saved in fullYearProfileData. If not, initialise object, else use session object
+  if (isFalsey(req.session.weeklyProfileData)) {
+    fullYearProfileData = setUpFullYearProfileFromSessionData();
+  } else {
+    fullYearProfileData = setUpInitialFullYearProfile(whpFlatProfile, pscFlatProfile);
   };
 
   var weeklyProfileData = {
@@ -148,65 +86,17 @@ function districtWeeklyProfilePage (req, res) {
     profileYear : req.session.profileData.profileYear ? req.session.profileData.profileYear : "2017/2018",
     whpProfile : req.session.profileData.whpProfile ? req.session.profileData.whpProfile : 1300,
     pscProfile : req.session.profileData.pscProfile ? req.session.profileData.pscProfile : 100,
-    week1whpProfile : week1whpProfile,
-    week2whpProfile : week2whpProfile,
-    week3whpProfile : week3whpProfile,
-    week4whpProfile : week4whpProfile,
-    week5whpProfile : week5whpProfile,
-    week52whpProfile : week52whpProfile,
-    week1pscProfile : week1pscProfile,
-    week2pscProfile : week2pscProfile,
-    week3pscProfile : week3pscProfile,
-    week4pscProfile : week4pscProfile,
-    week5pscProfile : week5pscProfile,
-    week52pscProfile : week52pscProfile,
-    week1whpExtrasProfile : week1whpExtrasProfile,
-    week2whpExtrasProfile : week2whpExtrasProfile,
-    week3whpExtrasProfile : week3whpExtrasProfile,
-    week4whpExtrasProfile : week4whpExtrasProfile,
-    week5whpExtrasProfile : week5whpExtrasProfile,
-    week52whpExtrasProfile : week52whpExtrasProfile,
-    week1pscExtrasProfile : week1pscExtrasProfile,
-    week2pscExtrasProfile : week2pscExtrasProfile,
-    week3pscExtrasProfile : week3pscExtrasProfile,
-    week4pscExtrasProfile : week4pscExtrasProfile,
-    week5pscExtrasProfile : week5pscExtrasProfile,
-    week52pscExtrasProfile : week52pscExtrasProfile
+    fullYearProfileData : fullYearProfileData
   };
 
   res.render('latest/whp-weekly-profile', weeklyProfileData);
 }
 
 function districtWeeklyProfileAction (req, res) {
-  
-  var inputWeeklyProfileData = {
-    week1whpProfile : req.body.week1whpProfile,
-    week2whpProfile : req.body.week2whpProfile,
-    week3whpProfile : req.body.week3whpProfile,
-    week4whpProfile : req.body.week4whpProfile,
-    week5whpProfile : req.body.week5whpProfile,
-    week52whpProfile : req.body.week52whpProfile,
-    week1pscProfile : req.body.week1pscProfile,
-    week2pscProfile : req.body.week2pscProfile,
-    week3pscProfile : req.body.week3pscProfile,
-    week4pscProfile : req.body.week4pscProfile,
-    week5pscProfile : req.body.week5pscProfile,
-    week52pscProfile : req.body.week52pscProfile,
-    week1whpExtrasProfile : req.body.week1whpExtras,
-    week2whpExtrasProfile : req.body.week2whpExtras,
-    week3whpExtrasProfile : req.body.week3whpExtras,
-    week4whpExtrasProfile : req.body.week4whpExtras,
-    week5whpExtrasProfile : req.body.week5whpExtras,
-    week52whpExtrasProfile : req.body.week52whpExtras,
-    week1pscExtrasProfile : req.body.week1pscExtras,
-    week2pscExtrasProfile : req.body.week2pscExtras,
-    week3pscExtrasProfile : req.body.week3pscExtras,
-    week4pscExtrasProfile : req.body.week4pscExtras,
-    week5pscExtrasProfile : req.body.week5pscExtras,
-    week52pscExtrasProfile : req.body.week52pscExtras
-  };
 
-  req.session.weeklyProfileData = inputWeeklyProfileData;
+  let inputFullYearProfileData = getFullYearProfileDataFromRequestBody();
+
+  req.session.weeklyProfileData.fullYearProfileData = inputFullYearProfileData;
   res.redirect('/latest/gatekeeper/weeklyProfile');
 }
 
@@ -222,10 +112,59 @@ function viewAllocationsPage (req,res) {
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-/*                                        Utilities
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-*/
+ /*                                        Utilities
+ /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
 
+function setUpInitialFullYearProfile(whpFlatProfile, pscFlatProfile) {
+  let i;
+  let fullYearProfileData = [];
+  for (i = 0; i = 52; i++) {
+    let weekNum = i + 1;
+    let weekProfileData = {
+      weekNum : weekNum,
+      weekwhpProfile : whpFlatProfile,
+      weekpscProfile : pscFlatProfile,
+      weekwhpExtrasProfile : 0,
+      weekpscExtrasProfile : 0
+    };
+    fullYearProfileData.push(weekProfileData);
+  }
+  return fullYearProfileData;
+}
+
+function setUpFullYearProfileFromSessionData() {
+  let j;
+  let fullYearProfileData = [];
+  for (j = 0; j = 52; j++) {
+    let weekProfileData = {
+      weekNum : req.session.weeklyProfileData.fullYearProfileData[j].weekNum,
+      weekwhpProfile : req.session.weeklyProfileData.fullYearProfileData[j].weekwhpProfile,
+      weekpscProfile : req.session.weeklyProfileData.fullYearProfileData[j].weekpscProfile,
+      weekwhpExtrasProfile : req.session.weeklyProfileData.fullYearProfileData[j].weekwhpExtrasProfile,
+      weekpscExtrasProfile : req.session.weeklyProfileData.fullYearProfileData[j].weekpscExtrasProfile
+    };
+    fullYearProfileData.push(weekProfileData);
+  }
+  return fullYearProfileData;
+}
+
+function getFullYearProfileDataFromRequestBody() {
+  let k;
+  let fullYearProfileData = [];
+  for (k = 0; k = 52; k++) {
+    let weekNum = k + 1;
+    let weekProfileData = {
+      weekNum : weekNum,
+      weekwhpProfile : req.body.weeklyProfileData.fullYearProfileData[k]['week' + weekNum + 'whpProfile'],
+      weekpscProfile : req.body.weeklyProfileData.fullYearProfileData[k]['week' + weekNum + 'pscProfile'],
+      weekwhpExtrasProfile : req.body.weeklyProfileData.fullYearProfileData[k]['week' + weekNum + 'whpExtrasProfile'],
+      weekpscExtrasProfile : req.body.weeklyProfileData.fullYearProfileData[k]['week' + weekNum + 'pscExtrasProfile']
+    }
+    fullYearProfileData.push(weekProfileData);
+  }
+  return fullYearProfileData;
+}
 
 function calcPercentAndFloor (num, perNum) {
   return Math.floor(num * perNum / 100);
@@ -235,10 +174,13 @@ function calcPercentAndRound (num, perNum) {
   return Math.round(num * perNum / 100);
 }
 
+function isFalsey (testValue) {
+  return (testValue === undefined || testValue == null || testValue.length <= 0) ? true : false;
+}
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-/*                                        Module Exports
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-*/
+ /*                                        Module Exports
+ /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
 
 module.exports.districtProfilePage = districtProfilePage;
 module.exports.districtProfileAction = districtProfileAction;
