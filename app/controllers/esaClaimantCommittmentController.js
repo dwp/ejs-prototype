cb4e843b9a7bab99195d2b4cc00856d6cd69f031
jegsivam <jane.gleadall.java@gmail.com>
2017-06-19 15:54:42 +0100

    Initial work on ESA Claimant Committment

--- app/controllers/esaClaimantCommittmentController.js
+++ app/controllers/esaClaimantCommittmentController.js
@@ -3,30 +3,117 @@
  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 
-function committmentActionsPage (req, res) {
-  var sessionData = req.session.committmentData || {};
+function viewCommittment (req, res) {
+  var newData = {};
+  var committmentDisplayObject = {};
 
-  var newName = sessionData.name || "Jiminy Cricket";
-  var newNino = sessionData.nino || "DE987654F";
+  if (!req.session.sessionData) {
+    var actionData = [
+      {
+        actionNum : 1,
+        action : "Default action 1",
+        how : "Deafult how 1",
+        byWhen : {
+          whenDay : "31",
+          whenMonth : getMonth(12),
+          whenYear : "2017"
+        },
+        volOrMand : "Voluntary"
+      },
+      {
+        actionNum : 2,
+        action : "Default action 2",
+        how : "Default how 2",
+        byWhen : {
+          whenDay : "31",
+          whenMonth : getMonth(12),
+          whenYear : "2017"
+        },
+        volOrMand : "Mandatory"
+      },
+      {
+        actionNum : 3,
+        action : "Default action 3",
+        how : "Default how 3",
+        byWhen : {
+          whenDay : "31",
+          whenMonth : getMonth(12),
+          whenYear : "2017"
+        },
+        volOrMand : "Mandatory"
+      }
+    ];
+    newData = actionData;
+  } else {
+    newData = req.session.sessionData;
+  }
+  committmentDisplayObject = {
+    name : "Justin Bimbolake",
+    nino : "AB123456C",
+    wca : "Yes",
+    actionData : newData
+  }
+  res.render('latest/esa-claimant-committment-view', committmentDisplayObject);
+}
 
-  var newData = {
-    name : newName,
-    nino : newNino
+function addClaimantCommittmentPage (req, res) {
+
+  newData = {
+    name : "James Cricket Esq.",
+    nino : "XY987654Z"
   }
 
   res.render('latest/esa-claimant-committment', newData);
 }
 
-function committmentActionsAction (req, res) {
+function addClaimantCommittmentAction (req, res) {
 
-  var getEsaCommittmentData = {
-    name  : "Jane Gleadall",
-    nino  : "AB123456C"
-  };
+  var claimantCommittmentData = [];
 
-  req.session.sessionData = getEsaCommittmentData;
-  res.redirect('/latest/esa_claimant/actions');
+  for (var i = 0; i < 9; i++) {
+    if (req.body['action-' + (i + 1)] !== '') {
+      var actionDay = req.body['whenDay-' + (i + 1)];
+      var actionMonth = req.body['whenMonth-' + (i + 1)];
+      var actionYear = req.body['whenYear-' + (i + 1)];
+      var action = {
+        actionNum : (i + 1),
+        action : req.body['action-' + (i + 1)],
+        how : req.body['how-' + (i + 1)],
+        byWhen : {
+          whenDay : parseInt(actionDay),
+          whenMonth : getMonth(parseInt(actionMonth)),
+          whenYear : parseInt(actionYear)
+        }
+      };
+      claimantCommittmentData.push(action);
+    }
+  }
+
+  req.session.sessionData = claimantCommittmentData;
+  res.redirect('/latest/esa_claimant/viewCommittment');
 }
 
-module.exports.committmentActionsPage = committmentActionsPage;
-module.exports.committmentActionsAction = committmentActionsAction;
+function getMonth(monthNumber) {
+
+  var textMonth;
+  var month = new Array()
+  month[0] = 'January';
+  month[1] = 'February';
+  month[2] = 'March';
+  month[3] = 'April';
+  month[4] = 'May';
+  month[5] = 'June';
+  month[6] = 'July';
+  month[7] = 'August';
+  month[8] = 'September';
+  month[9] = 'October';
+  month[10] = 'November';
+  month[11] = 'December';
+
+  textMonth = month[monthNumber - 1];
+  return textMonth;
+}
+
+module.exports.viewCommittment = viewCommittment;
+module.exports.addClaimantCommittmentPage = addClaimantCommittmentPage;
+module.exports.addClaimantCommittmentAction = addClaimantCommittmentAction;
--- app/routes/esa-claimant-committment.js
+++ app/routes/esa-claimant-committment.js
@@ -2,7 +2,9 @@ var esaClaimant = require("../controllers/esaClaimantCommittmentController");
 
 module.exports = function(router) {
 
-  router.get('/latest/esa_claimant/actions', esaClaimant.committmentActionsPage);
-  router.post('/latest/esa_claimant/actions', esaClaimant.committmentActionsAction);
+  router.get('/latest/esa_claimant/addCommittment', esaClaimant.addClaimantCommittmentPage);
+  router.post('/latest/esa_claimant/addCommittment', esaClaimant.addClaimantCommittmentAction);
+
+  router.get('/latest/esa_claimant/viewCommittment', esaClaimant.viewCommittment);
 
 }
