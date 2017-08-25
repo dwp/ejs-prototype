e54bc736ad79f3e88eec6840668d2975b347084e
jegsivam <jane.gleadall.java@gmail.com>
2017-08-25 16:29:56 +0100

    DO NOT MERGE YET: More amendments. Still not working corrcetly

--- app/controllers/appointmentController.js
+++ app/controllers/appointmentController.js
@@ -5,7 +5,8 @@
 
 function appointmentsPage(req, res) {
 
-  req.session.appointments = req.session.appointments || setInitialAppointments();
+  var appointments = setInitialAppointments();
+  req.session.appointments = req.session.appointments || appointments;
   res.locals.data.appointments = req.session.appointments;
   res.render('latest/appointments');
 }
@@ -18,6 +19,7 @@ function appointmentEditPage(req, res) {
   if (req.query.id) {
     res.locals.data.appointmentForUpdate = appointments[req.query.id];
   } else {
+    res.locals.data.newAppt = 1;
     res.locals.data.appointmentForUpdate = {}
   }
 
@@ -41,8 +43,10 @@ function appointmentEditPageAction(req, res) {
   if (req.query.id) {
     index = appointments.indexOf(req.body.id);
     appointment.id = req.body.id;
+    appointment.apptStatus = req.body['appt-status'];
     appointments[index] = appointment;
   } else {
+    appointment.apptStatus = 'Booked';
     appointments.unshift(appointment);
   }
 
@@ -53,10 +57,10 @@ function appointmentEditPageAction(req, res) {
 
 function setInitialAppointments(){
   return [
-    { id: 0, apptType: 'Appointment type 4', apptDateValues: [ '01', '10', '2017'], apptStatus: 'Confirmed', apptTimeHrs: '10', apptTimeMins: '15'},
-    { id: 1, apptType: 'Appointment type 3', apptDateValues: [ '01', '07', '2017'], apptStatus: 'Failed to attend', apptTimeHrs: '14', apptTimeMins: '10'},
-    { id: 2, apptType: 'Appointment type 1', apptDateValues: [ '01', '06', '2017'], apptStatus: 'Re-booked', apptTimeHrs: '11', apptTimeMins: '05'},
-    { id: 3, apptType: 'Appointment type 1', apptDateValues: [ '01', '04', '2017'], apptStatus: 'Attended', apptTimeHrs: '14', apptTimeMins: '45'}
+    { id: 1, apptType: 'Appointment type 4', apptDateValues: [ '01', '10', '2017'], apptStatus: 'Confirmed', apptTimeHrs: '10', apptTimeMins: '15'},
+    { id: 2, apptType: 'Appointment type 3', apptDateValues: [ '01', '07', '2017'], apptStatus: 'Failed to attend', apptTimeHrs: '14', apptTimeMins: '10'},
+    { id: 3, apptType: 'Appointment type 1', apptDateValues: [ '01', '06', '2017'], apptStatus: 'Re-booked', apptTimeHrs: '11', apptTimeMins: '05'},
+    { id: 4, apptType: 'Appointment type 1', apptDateValues: [ '01', '04', '2017'], apptStatus: 'Attended', apptTimeHrs: '14', apptTimeMins: '45'}
   ];
 }
 
